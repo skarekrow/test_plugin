e1c3a657cafc949dd7b53d10ae4d3a42e32b3507
Brandon Schneider <brandon@ixsystems.com>
2017-08-23 13:18:42 -0500

    Flip to a more real world issue

--- post_install.sh
+++ post_install.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+fetch http://www.tolisgroup.com/downloads/bruserver20/complete/BRU_Server_Complete_freebsd11_2.0.5foo.tgz
+tar xvfz BRU_Server_Complete_freebsd11_2.0.5.tgz
+mkdir -p /usr/local/bru-server
+mkdir -p /usr/local/bin
+mkdir -p /usr/local/etc/rc.d
+cd unix/agent && mv * /usr/local/bru-server && cd ../console && mv * /usr/local/bru-server
+chmod +x /usr/local/etc/rc.d/bru_server
+ln -s /usr/local/bru-server/bru-server.console /usr/local/bin/bru-server.console
+sysrc -f /etc/rc.conf bru_server_enable="YES"
 
-exit 1
-
-# Enable the service
-sysrc -f /etc/rc.conf btsync_enable="YES"
-
-# Start the service
-service btsync start 2>/dev/null
+echo "You will need to set bru_server_server and bru_server_password in /etc/rc.conf before the service will start"
+echo "For more information on how to use BRU, visit: http://www.tolisgroup.com/assets/bru_server_1.2.0_cli_guide.pdf"
